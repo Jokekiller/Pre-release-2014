554230be77320830c8f84f19f768d38804156c7e
Harry <Harry@192.168.0.15>
2015-03-23 19:28:53 +0000

    Pre release actual files

---
+++ COMP1_Summer_2014_SkelProg_Python32Pub0.0.0_version2.py
@@ -0,0 +1,254 @@
+# Skeleton Program code for the AQA COMP1 Summer 2014 examination
+# this code should be used in conjunction with the Preliminary Material
+# written by the AQA Programmer Team
+# developed in the Python 3.2 programming environment
+# version 2 edited 06/03/2014
+
+import random
+from datetime import *
+
+NO_OF_RECENT_SCORES = 3
+
+class TCard():
+  def __init__(self):
+    self.Suit = 0
+    self.Rank = 0
+
+class TRecentScore():
+  def __init__(self):
+    self.Name = ''
+    self.Score = 0
+    self.Date = None
+
+Deck = [None]
+RecentScores = [None]
+Choice = ''
+
+def GetRank(RankNo):
+  Rank = ''
+  if RankNo == 1:
+    Rank = 'Ace'
+  elif RankNo == 2:
+    Rank = 'Two'
+  elif RankNo == 3:
+    Rank = 'Three'
+  elif RankNo == 4:
+    Rank = 'Four'
+  elif RankNo == 5:
+    Rank = 'Five'
+  elif RankNo == 6:
+    Rank = 'Six'
+  elif RankNo == 7:
+    Rank = 'Seven'
+  elif RankNo == 8:
+    Rank = 'Eight'
+  elif RankNo == 9:
+    Rank = 'Nine'
+  elif RankNo == 10:
+    Rank = 'Ten'
+  elif RankNo == 11:
+    Rank = 'Jack'
+  elif RankNo == 12:
+    Rank = 'Queen'
+  elif RankNo == 13:
+    Rank = 'King'
+  return Rank
+
+def GetSuit(SuitNo):
+  Suit = ''
+  if SuitNo == 1:
+    Suit = 'Clubs'
+  elif SuitNo == 2:
+    Suit = 'Diamonds'
+  elif SuitNo == 3:
+    Suit = 'Hearts'
+  elif SuitNo == 4:
+    Suit = 'Spades'
+  return Suit
+
+def DisplayMenu():
+  print()
+  print('MAIN MENU')
+  print()
+  print('1. Play game (with shuffle)')
+  print('2. Play game (without shuffle)')
+  print('3. Display recent scores')
+  print('4. Reset recent scores')
+  print()
+  print('Select an option from the menu (or enter q to quit): ', end='')
+
+def GetMenuChoice():
+  Choice = ''
+  while Choice != 'q'and Choice != 'Q' and Choice != 'Quit' and Choice != '1' and Choice != '2' and Choice != '3' and Choice != '4':
+    Choice = input()
+    print()
+  return Choice
+
+def LoadDeck(Deck):
+  CurrentFile = open('deck.txt', mode='r',encoding = 'utf-8')
+  Count = 1
+  while True:
+    LineFromFile = CurrentFile.readline()
+    if not LineFromFile:
+      CurrentFile.close()
+      break
+    Deck[Count].Suit = int(LineFromFile)
+    LineFromFile = CurrentFile.readline()
+    Deck[Count].Rank = int(LineFromFile)
+    Count = Count + 1
+ 
+def ShuffleDeck(Deck):
+  SwapSpace = TCard()
+  NoOfSwaps = 1000
+  for NoOfSwapsMadeSoFar in range(1, NoOfSwaps + 1):
+    Position1 = random.randint(1, 52)
+    Position2 = random.randint(1, 52)
+    SwapSpace.Rank = Deck[Position1].Rank
+    SwapSpace.Suit = Deck[Position1].Suit
+    Deck[Position1].Rank = Deck[Position2].Rank
+    Deck[Position1].Suit = Deck[Position2].Suit
+    Deck[Position2].Rank = SwapSpace.Rank
+    Deck[Position2].Suit = SwapSpace.Suit
+
+def DisplayCard(ThisCard):
+  print()
+  print('Card is the', GetRank(ThisCard.Rank), 'of', GetSuit(ThisCard.Suit))
+  print()
+
+def GetCard(ThisCard, Deck, NoOfCardsTurnedOver):
+  ThisCard.Rank = Deck[1].Rank
+  ThisCard.Suit = Deck[1].Suit
+  for Count in range(1, 52 - NoOfCardsTurnedOver):
+    Deck[Count].Rank = Deck[Count + 1].Rank
+    Deck[Count].Suit = Deck[Count + 1].Suit
+  Deck[52 - NoOfCardsTurnedOver].Suit = 0
+  Deck[52 - NoOfCardsTurnedOver].Rank = 0
+
+def IsNextCardHigher(LastCard, NextCard):
+  Higher = False
+  if NextCard.Rank > LastCard.Rank:
+    Higher = True
+  return Higher
+
+def GetPlayerName():
+  print()
+  PlayerName = ''
+  while PlayerName == '':
+    PlayerName = input('Please enter your name: ')
+    print()
+  return PlayerName
+
+def GetChoiceFromUser():
+  Choice = ''
+  while (Choice != 'y') and (Choice != 'Y') and (Choice != 'yes') and (Choice != 'Yes') and (Choice != 'n') and (Choice != 'N') and (Choice != 'no') and (Choice != 'No'):
+    Choice = input('Do you think the next card will be higher than the last card (enter y or n)? ')
+    if (Choice != 'y') and (Choice != 'Y') and (Choice != 'yes') and (Choice != 'Yes') and (Choice != 'n') and (Choice != 'N') and (Choice != 'no') and (Choice != 'No'):
+      print("Invalid input")
+  return Choice
+
+def DisplayEndOfGameMessage(Score):
+  print()
+  print('GAME OVER!')
+  print('Your score was', Score)
+  if Score == 51:
+    print('WOW! You completed a perfect game.')
+  print()
+  currentDate = datetime.now()
+  print("Date achieved {0}".format(currentDate))
+
+def DisplayCorrectGuessMessage(Score):
+  print()
+  print('Well done! You guessed correctly.')
+  print('Your score is now ', Score, '.', sep='')
+  print()
+
+def ResetRecentScores(RecentScores):
+  currentDate = datetime.now()
+  for Count in range(1, NO_OF_RECENT_SCORES + 1):
+    RecentScores[Count].Name = ''
+    RecentScores[Count].Score = 0
+    RecentScores[Count].Date = ''
+
+def DisplayRecentScores(RecentScores):
+  currentDate = datetime.now()
+  print()
+  print('Recent Scores: ')
+  print()
+  print("|{0:^10}|{1:^20}|{2:^20}|".format("Name", "Score", "Date"))
+  for Count in range(1, NO_OF_RECENT_SCORES + 1):
+    currentString = datetime.strftime(currentDate, "%d/%m/%Y")
+    print("-"*50)
+    print("|{0:^5} got a score of: {1:^10} on the {2:^10}|".format(RecentScores[Count].Name, RecentScores[Count].Score, currentString))
+  print()
+  print('Press the Enter key to return to the main menu')
+  input()
+  print()
+
+def UpdateRecentScores(RecentScores, Score):
+  PlayerName = GetPlayerName()
+  recordScore = input("Do you want to add your score to the high scores table?(y/n):")
+  currentDate = datetime.now()
+  FoundSpace = False
+  Count = 1
+  if recordScore == "y" or recordScore == "Y" or recordScore == "yes" or recordScore == "Yes":
+    while (not FoundSpace) and (Count <= NO_OF_RECENT_SCORES):
+      if RecentScores[Count].Name == '':
+        FoundSpace = True
+      else:
+        Count = Count + 1
+    if not FoundSpace:
+      for Count in range(1, NO_OF_RECENT_SCORES):
+        RecentScores[Count].Name = RecentScores[Count + 1].Name
+        RecentScores[Count].Score = RecentScores[Count + 1].Score
+        RecentScores[Count].Date = RecentScores[Count + 1].Date
+      Count = NO_OF_RECENT_SCORES
+    RecentScores[Count].Name = PlayerName
+    RecentScores[Count].Score = Score
+    RecentScores[Count].Date = currentDate
+
+def PlayGame(Deck, RecentScores):
+  LastCard = TCard()
+  NextCard = TCard()
+  GameOver = False
+  GetCard(LastCard, Deck, 0)
+  DisplayCard(LastCard)
+  NoOfCardsTurnedOver = 1
+  while (NoOfCardsTurnedOver < 52) and (not GameOver):
+    GetCard(NextCard, Deck, NoOfCardsTurnedOver)
+    Choice = GetChoiceFromUser()
+    DisplayCard(NextCard)
+    NoOfCardsTurnedOver = NoOfCardsTurnedOver + 1
+    Higher = IsNextCardHigher(LastCard, NextCard)
+    if (Higher and Choice == 'y') or (not Higher and Choice == 'n'):
+      DisplayCorrectGuessMessage(NoOfCardsTurnedOver - 1)
+      LastCard.Rank = NextCard.Rank
+      LastCard.Suit = NextCard.Suit
+    else:
+      GameOver = True
+  if GameOver:
+    DisplayEndOfGameMessage(NoOfCardsTurnedOver - 2)
+    UpdateRecentScores(RecentScores, NoOfCardsTurnedOver - 2)
+  else:
+    DisplayEndOfGameMessage(51)
+    UpdateRecentScores(RecentScores, 51)
+
+if __name__ == '__main__':
+  for Count in range(1, 53):
+    Deck.append(TCard())
+  for Count in range(1, NO_OF_RECENT_SCORES + 1):
+    RecentScores.append(TRecentScore())
+  Choice = ''
+  while Choice != 'q':
+    DisplayMenu()
+    Choice = GetMenuChoice()
+    if Choice == '1':
+      LoadDeck(Deck)
+      ShuffleDeck(Deck)
+      PlayGame(Deck, RecentScores)
+    elif Choice == '2':
+      LoadDeck(Deck)
+      PlayGame(Deck, RecentScores)
+    elif Choice == '3':
+      DisplayRecentScores(RecentScores)
+    elif Choice == '4':
+      ResetRecentScores(RecentScores)
